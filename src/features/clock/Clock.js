012d3c1e2e582bdce6ef10474db5ac006576bcf1
mshuber1981 <michael.huber1981@outlook.com>
2020-11-13 09:15:39 -0600

    Updating Clock content

--- src/features/clock/Clock.js
+++ src/features/clock/Clock.js
@@ -1,12 +1,137 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+// https://react-bootstrap.github.io/components/jumbotron/
+import { Jumbotron } from "react-bootstrap";
+// https://react-icons.netlify.com/#/
+import { FcPlus, FcMinus } from "react-icons/fc";
 import { useSelector, useDispatch } from "react-redux";
-import { clockSelector } from "./clockSlice";
+import {
+  clockSelector,
+  setBrkLength,
+  setSeshLength,
+  start,
+  stop,
+  setIntervalID,
+  decrementTimer,
+  reset,
+} from "./clockSlice";
 
 const Clock = () => {
-  const {} = useSelector(clockSelector);
+  const { brkLength, seshLength, timerType, timer, timerState } = useSelector(
+    clockSelector
+  );
   const dispatch = useDispatch();
+  const clock = () => {
+    let minutes = Math.floor(timer / 60);
+    let seconds = timer - minutes * 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    // setCurrentTimer(minutes + ":" + seconds);
+    return minutes + ":" + seconds;
+  };
+  const timerControl = () => {
+    if (timerState === "stopped") {
+      dispatch(start());
+      dispatch(
+        setIntervalID(
+          setInterval(() => {
+            dispatch(decrementTimer());
+          }, 1000)
+        )
+      );
+    } else {
+      dispatch(stop());
+    }
+  };
+  // https://reactjs.org/docs/refs-and-the-dom.html
+  let audioBeep = useRef(null);
+  const resetAll = () => {
+    dispatch(reset());
+    audioBeep.pause();
+    audioBeep.currentTime = 0;
+  };
+  useEffect(() => {
+    let currentTimer = timer;
+    if (currentTimer === 0) {
+      audioBeep.play();
+    }
+  }, [timer]);
 
-  return <></>;
+  return (
+    <Jumbotron className="clock text-center">
+      <div className="d-flex justify-content-around mb-5">
+        <h3 id="break-label" className="">
+          Break Length
+          <div className="d-flex flex-column text-center">
+            <div id="break-length" className="d-inline">
+              <button
+                id="break-increment"
+                value="+"
+                onClick={(event) =>
+                  dispatch(setBrkLength(event.currentTarget.value))
+                }
+              >
+                <FcPlus />
+              </button>
+              {brkLength}
+              <button
+                id="break-decrement"
+                value="-"
+                onClick={(event) =>
+                  dispatch(setBrkLength(event.currentTarget.value))
+                }
+              >
+                <FcMinus />
+              </button>
+            </div>
+          </div>
+        </h3>
+
+        <h3 id="session-label" className="">
+          Session Length
+          <div className="d-flex flex-column text-center">
+            <div id="session-length" className="d-inline">
+              <button
+                id="session-increment"
+                value="+"
+                onClick={(event) =>
+                  dispatch(setSeshLength(event.currentTarget.value))
+                }
+              >
+                <FcPlus />
+              </button>
+              {seshLength}
+              <button
+                id="session-decrement"
+                value="-"
+                onClick={(event) =>
+                  dispatch(setSeshLength(event.currentTarget.value))
+                }
+              >
+                <FcMinus />
+              </button>
+            </div>
+          </div>
+        </h3>
+      </div>
+
+      <h3 id="timer-label">{timerType}</h3>
+      <h3 id="time-left">{clock()}</h3>
+      <button id="start_stop" onClick={() => timerControl()}>
+        Start/Stop
+      </button>
+      <button id="reset" onClick={() => resetAll()}>
+        Reset
+      </button>
+      <audio
+        id="beep"
+        preload="auto"
+        ref={(audio) => {
+          audioBeep = audio;
+        }}
+        src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+      />
+    </Jumbotron>
+  );
 };
 
 export default Clock;
--- src/features/clock/clockSlice.js
+++ src/features/clock/clockSlice.js
@@ -3,11 +3,109 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const clockSlice = createSlice({
   name: "clock",
-  initialState: {},
-  reducers: {},
+  initialState: {
+    brkLength: 5,
+    seshLength: 25,
+    timerState: "stopped",
+    timerType: "Session",
+    timer: 1500,
+    intervalID: "",
+    alarmColor: { color: "white" },
+  },
+  reducers: {
+    setBrkLength: (state, { payload }) => {
+      if (state.timerState === "running") {
+        return;
+      }
+      if (state.timerType === "Session") {
+        if (payload === "-" && state.brkLength !== 1) {
+          state.brkLength = state.brkLength - 1;
+        } else if (payload === "+" && state.brkLength !== 60) {
+          state.brkLength = state.brkLength + 1;
+        }
+      } else if (payload === "-" && state.brkLength !== 1) {
+        state.brkLength = state.brkLength - 1;
+        state.timer = state.brkLength * 60;
+      } else if (payload === "+" && state.brkLength !== 60) {
+        state.brkLength = state.brkLength + 1;
+        state.timer = state.brkLength * 60;
+      }
+    },
+    setSeshLength: (state, { payload }) => {
+      if (state.timerState === "running") {
+        return;
+      }
+      if (state.timerType === "Break") {
+        if (payload === "-" && state.seshLength !== 1) {
+          state.seshLength = state.seshLength - 1;
+        } else if (payload === "+" && state.seshLength !== 60) {
+          state.seshLength = state.seshLength + 1;
+        }
+      } else if (payload === "-" && state.seshLength !== 1) {
+        state.seshLength = state.seshLength - 1;
+        state.timer = state.seshLength * 60;
+      } else if (payload === "+" && state.seshLength !== 60) {
+        state.seshLength = state.seshLength + 1;
+        state.timer = state.seshLength * 60;
+      }
+    },
+    start: (state) => {
+      state.timerState = "running";
+    },
+    stop: (state) => {
+      state.timerState = "stopped";
+    },
+    setIntervalID: (state, { payload }) => {
+      state.intervalID = payload;
+    },
+    decrementTimer: (state) => {
+      if (state.timerState === "running" && state.timerType === "Session") {
+        if (state.timer === 0) {
+          state.timerType = "Break";
+          state.timer = state.brkLength * 60;
+          // state.timer = state.timer - 1;
+        } else {
+          state.timer = state.timer - 1;
+        }
+      } else if (
+        state.timerState === "running" &&
+        state.timerType === "Break"
+      ) {
+        if (state.timer === 0) {
+          state.timerType = "Session";
+          state.timer = state.seshLength * 60;
+          // state.timer = state.timer - 1;
+        } else {
+          state.timer = state.timer - 1;
+        }
+      } else {
+        clearInterval(state.intervalID);
+      }
+    },
+    reset: (state) => {
+      state.brkLength = 5;
+      state.seshLength = 25;
+      state.timerState = "stopped";
+      state.timerType = "Session";
+      state.timer = 1500;
+      state.alarmColor = { color: "white" };
+      if (state.intervalID) {
+        clearInterval(state.intervalID);
+        state.intervalID = "";
+      }
+    },
+  },
 });
 
-export const {} = clockSlice.actions;
+export const {
+  setBrkLength,
+  setSeshLength,
+  start,
+  stop,
+  setIntervalID,
+  decrementTimer,
+  reset,
+} = clockSlice.actions;
 
 export const clockSelector = (state) => state.clock;
 
