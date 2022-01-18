1a31e2262c2d00293912bd1d1956378a6e1891d3
Michael Huber <michael.huber1981@outlook.com>
2022-01-18 08:03:00 -0600

    Updating marked import

--- src/features/markdown/Markdown.js
+++ src/features/markdown/Markdown.js
@@ -1,8 +1,8 @@
 import React from "react";
 // https://www.npmjs.com/package/marked
-import marked from "marked";
+import { marked } from "marked";
 // https://react-bootstrap.github.io/layout/grid/#responsive-grids
-import {Row, Col} from "react-bootstrap"
+import { Row, Col } from "react-bootstrap";
 // https://react-redux.js.org/api/hooks#using-hooks-in-a-react-redux-app
 import { useSelector, useDispatch } from "react-redux";
 import { handleMarkdownChange, markdownSelector } from "./markdownSlice";
@@ -17,23 +17,23 @@ const Markdown = () => {
 
   return (
     <Row className="my-5 py-5">
-      <Col className="bg-light" lg={6} >
-      <textarea
-        id="editor"
-        className="h-100 w-100 py-5"
-        onChange={(event) => dispatch(handleMarkdownChange(event))}
-        type="text"
-        value={markdown}
-      />
+      <Col className="bg-light" lg={6}>
+        <textarea
+          id="editor"
+          className="h-100 w-100 py-5"
+          onChange={(event) => dispatch(handleMarkdownChange(event))}
+          type="text"
+          value={markdown}
+        />
       </Col>
-      <Col className="bg-light" lg={6} >
-      <div
-        id="preview"
-        className="py-5"
-        dangerouslySetInnerHTML={{
-          __html: marked(markdown),
-        }}
-      />
+      <Col className="bg-light" lg={6}>
+        <div
+          id="preview"
+          className="py-5"
+          dangerouslySetInnerHTML={{
+            __html: marked(markdown),
+          }}
+        />
       </Col>
     </Row>
   );
