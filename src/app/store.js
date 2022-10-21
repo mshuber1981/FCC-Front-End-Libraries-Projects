32ebf83584d5aa7f0c4bb8b3d72a35f1bd4b3c30
Michael Huber <michael.huber1981@outlook.com>
2022-10-21 08:59:18 -0500

    Updating getDefaultMiddlware method

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 // https://redux-toolkit.js.org/api/configureStore#configurestore
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import quotesReducer from "../features/quotes/quotesSlice";
 import markdownReducer from "../features/markdown/markdownSlice";
 import drumReducer from "../features/drum/drumSlice";
@@ -8,11 +8,12 @@ import clockReducer from "../features/clock/clockSlice";
 
 export default configureStore({
   // https://redux-toolkit.js.org/usage/usage-guide#working-with-non-serializable-data
-  middleware: getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: ["markdown/handleMarkdownChange"],
-    },
-  }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["markdown/handleMarkdownChange"],
+      },
+    }),
   reducer: {
     quotes: quotesReducer,
     markdown: markdownReducer,
