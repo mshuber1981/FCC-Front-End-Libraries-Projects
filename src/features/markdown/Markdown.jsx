3538f8cc4cab2580de53f5b8796527b3535515ae
Michael Huber <michael.huber1981@outlook.com>
2023-02-26 08:43:34 -0600

    Adding copy button for markdown

--- src/features/markdown/Markdown.jsx
+++ src/features/markdown/Markdown.jsx
@@ -6,31 +6,69 @@ import styled from "styled-components";
 // https://www.npmjs.com/package/marked
 import { marked } from "marked";
 // Components
-import { Row, Col } from "react-bootstrap";
+import { Button, Row, Col } from "react-bootstrap";
 
-const StyledTextArea = styled.textarea`
-  resize: none;
-  min-height: 10rem;
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  textarea {
+    min-height: 10rem;
+  }
+
+  button {
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+  }
 `;
 
 const Markdown = () => {
+  const [isCoppied, setCoppied] = React.useState(false);
+  const dispatch = useDispatch();
+  const { markdown } = useSelector(markdownSelector);
+
   marked.setOptions({
     breaks: true,
   });
 
-  const dispatch = useDispatch();
-  const { markdown } = useSelector(markdownSelector);
-
   return (
-    <Row className="">
-      <Col className="" lg={6}>
-        <StyledTextArea
-          id="editor"
-          className="h-100 w-100"
-          onChange={(event) => dispatch(handleMarkdownChange(event))}
-          type="text"
-          value={markdown}
-        />
+    <Row>
+      <Col lg={6}>
+        <StyledDiv>
+          <textarea
+            id="editor"
+            className="h-100 w-100 px-2"
+            onChange={(event) => dispatch(handleMarkdownChange(event))}
+            type="text"
+            value={markdown}
+          />
+          {isCoppied ? (
+            <Button
+              variant="success"
+              onClick={function () {
+                // Copy markdown to clipboard
+                navigator.clipboard.writeText(markdown);
+              }}
+            >
+              Copied
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              onClick={function () {
+                // Copy markdown to clipboard
+                navigator.clipboard.writeText(markdown);
+                setCoppied(true);
+                setTimeout(() => {
+                  setCoppied(false);
+                }, "5000");
+              }}
+            >
+              Copy Markdown
+            </Button>
+          )}
+        </StyledDiv>
       </Col>
       <Col className="" lg={6}>
         <div
